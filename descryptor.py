9cd86274c458f83eaf596741efa01246ec18885d
Marcin Drzewiecki <marcin9504@wp.pl>
2018-05-07 20:04:31 +0200

    descriptor: hu moments

--- descryptor.py
+++ descryptor.py
@@ -1,5 +1,8 @@
+import math
+
 import cv2
 import numpy as np
+
 
 def extract(img, points):
     descriptors = []
@@ -7,16 +10,36 @@ def extract(img, points):
     # sift = cv2.xfeatures2d.SIFT_create()
     for point in points:
         y, x = point
-        sample = img[y-32:y+32,x-32:x+32]
-        des = np.average(sample)
+        sample = img[y - 32:y + 32, x - 32:x + 32]
+        des = cv2.HuMoments(cv2.moments(img))
+        # des = np.average(sample)
         # kp, des = sift.detectAndCompute(sample, None)
         descriptors.append(des)
     return descriptors
 
 
 def distance(des1, des2):
-    print(des1)
-    print(des2)
-    # if np.array(des1).shape != np.array(des2).shape:
-    #     return 0
-    return np.abs(des1 - des2)
+    # print(des1)
+    # print(des2)
+    # return np.abs(des1 - des2)
+
+    s = 0
+    for i in range(len(des1)):
+        diff = abs(des1[i] - des2[i])
+        if diff == 0:
+            continue
+        num = -sign(diff) * math.log10(diff)
+        s += num
+
+    return s
+
+
+def sign(x):
+    if x > 0:
+        return 1.
+    elif x < 0:
+        return -1.
+    elif x == 0:
+        return 0.
+    else:
+        return x
