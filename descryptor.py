9bb100f7aec6b905aa9e5b0a7e528a2b4dbbeb96
Marcin Drzewiecki <marcin9504@wp.pl>
2018-05-07 23:56:09 +0200

    sift descriptors

--- descryptor.py
+++ descryptor.py
@@ -5,24 +5,43 @@ import numpy as np
 
 
 def extract(img, points):
+    # return hu_extract(img, points)
+    return sift_extract(img, points)
+
+
+def distance(des1, des2):
+    # return hu_distance(des1, des2)
+    return sift_distance(des1, des2)
+
+
+def sift_extract(img, points):
     descriptors = []
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # sift = cv2.xfeatures2d.SIFT_create()
+    sift = cv2.xfeatures2d.SIFT_create(400)
     for point in points:
         y, x = point
         sample = img[y - 32:y + 32, x - 32:x + 32]
-        des = cv2.HuMoments(cv2.moments(img))
-        # des = np.average(sample)
-        # kp, des = sift.detectAndCompute(sample, None)
+        kp, des = sift.detectAndCompute(sample, None)
         descriptors.append(des)
     return descriptors
 
 
-def distance(des1, des2):
-    # print(des1)
-    # print(des2)
-    # return np.abs(des1 - des2)
+def sift_distance(des1, des2):
+    threshold = 1 / 30
+    if des1 is None or des2 is None:
+        print("None")
+        return 1
+    bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+    matches = bf.match(des1, des2)
+    # matches = sorted(matches, key=lambda x: x.distance)
+    # print(matches)
+    # print(len(matches))
+    print(len(matches))
+    return threshold / len(matches)
 
+
+def hu_distance(des1, des2):
+    threshold = 50
     s = 0
     for i in range(len(des1)):
         diff = abs(des1[i] - des2[i])
@@ -30,8 +49,21 @@ def distance(des1, des2):
             continue
         num = -sign(diff) * math.log10(diff)
         s += num
+    if s < threshold:
+        return 0
+    else:
+        return 1
 
-    return s
+
+def hu_extract(img, points):
+    descriptors = []
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    for point in points:
+        y, x = point
+        sample = img[y - 32:y + 32, x - 32:x + 32]
+        des = cv2.HuMoments(cv2.moments(sample))
+        descriptors.append(des)
+    return descriptors
 
 
 def sign(x):
