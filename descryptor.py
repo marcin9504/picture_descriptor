ee7354552789f6b77f013fa881450cfa8fcd3b9c
Marcin Drzewiecki <marcin9504@wp.pl>
2018-05-08 20:16:09 +0200

    sampling and normalizing

--- descryptor.py
+++ descryptor.py
@@ -20,7 +20,7 @@ def sift_extract(img, points):
     sift = cv2.xfeatures2d.SIFT_create(400)
     for point in points:
         y, x = point
-        sample = img[y - 32:y + 32, x - 32:x + 32]
+        sample = get_sample(img, x, y)
         kp, des = sift.detectAndCompute(sample, None)
         descriptors.append(des)
     return descriptors
@@ -33,11 +33,10 @@ def sift_distance(des1, des2):
         return 1
     bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
     matches = bf.match(des1, des2)
+    # for match in matches:
+    #     print(match.distance)
     # matches = sorted(matches, key=lambda x: x.distance)
-    # print(matches)
-    # print(len(matches))
-    print(len(matches))
-    return threshold / len(matches)
+    return float(threshold) / len(matches)
 
 
 def hu_distance(des1, des2):
@@ -60,10 +59,21 @@ def hu_extract(img, points):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     for point in points:
         y, x = point
-        sample = img[y - 32:y + 32, x - 32:x + 32]
+        sample = get_sample(img, x, y)
         des = cv2.HuMoments(cv2.moments(sample))
         descriptors.append(des)
     return descriptors
+
+
+def normalise_image(img):
+    return cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
+
+
+def get_sample(img, x, y, r=32, normalize=True):
+    sample = img[y - r:y + r, x - r:x + r]
+    if normalize:
+        sample = normalise_image(sample)
+    return sample
 
 
 def sign(x):
