e429e624710d3755d6c6a4e05e97c169012944e2
Marcin Drzewiecki <marcin9504@wp.pl>
2018-05-14 18:34:19 +0200

    added max value on the circle

--- descryptor.py
+++ descryptor.py
@@ -22,6 +22,7 @@ def extract(img, points):
     des.append(circle_hist_extract(img, points))
     des.append(hu_extract(img, points))
     des.append(average_extract(img, points))
+    des.append(max_on_circle_extract(img, points))
     return des
 
 
@@ -31,12 +32,67 @@ def distance(des1, des2):
     scores.append(circle_hist_distance(des1[0], des2[0]))
     scores.append(hu_distance(des1[1], des2[1]))
     scores.append(average_distance(des1[2], des2[2]))
+    scores.append(max_on_circle_distance(des1[3], des2[3]))
 
     for idx, d in enumerate(scores):
         if d not in range(0, 1):
             scores[idx] = 0.5
 
     return sum(scores) / float(len(scores))
+
+
+def max_on_circle_extract(img, points):
+    descriptors = []
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    for point in points:
+        y, x = point
+        sample = get_sample(img, x, y)
+        w, h = sample.shape
+        dict_brightness = {}
+        center_x, center_y = w // 2, h // 2
+
+        for i in range(w):
+            for j in range(h):
+                key = math.floor(math.sqrt((i - center_x) ** 2 + (j - center_y) ** 2))
+                if key == 0:
+                    continue
+                val = dict_brightness.get(key, [0, 0])
+                if sample[i, j] > val[1]:
+                    val[1] = sample[i, j]
+                    sin = (j - center_y) / key
+                    alpha = math.asin(sin)
+                    val[0] = alpha
+
+                dict_brightness[key] = val
+        des = [0 for _ in range(len(dict_brightness))]
+        for key, value in dict_brightness.items():
+            des[key - 1] = value[0]
+
+        descriptors.append(des[:32])
+        # print(des)
+    return descriptors
+
+
+def max_on_circle_distance(des1, des2):
+    corr = []
+    for scale in (0.7, 0.8, 0.9, 1):
+        rescaled_length = math.floor(len(des1) * scale)
+        ten_degrees = math.pi / 36
+        for factor in range(0, 18):
+            factor = ten_degrees * factor
+            tmp_v = []
+            for i in des1:
+                value = i + factor
+                if value > math.pi / 2:
+                    value -= math.pi
+                tmp_v.append(value)
+            des1 = tmp_v
+            corr.append(abs(pearsonr(des1[:rescaled_length], rescale(des2, rescaled_length))[0]))
+            corr.append(abs(pearsonr(des2[:rescaled_length], rescale(des1, rescaled_length))[0]))
+    if np.isnan(max(corr)):
+        return 0.5
+    else:
+        return max(corr)
 
 
 def average_extract(img, points):
